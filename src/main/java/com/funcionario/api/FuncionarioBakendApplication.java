f19b41967c751f68ad406322dc3139c3652fd66a
aurelio <aurelio@aurelio-Lenovo-V15-G2-ALC>
2022-09-08 10:26:57 +0200

    teste

---
+++ src/main/java/com/funcionario/api/FuncionarioBakendApplication.java
@@ -0,0 +1,13 @@
+package com.funcionario.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionarioBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionarioBakendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funcionario/api/FuncionarioBakendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funcionario.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioBakendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
